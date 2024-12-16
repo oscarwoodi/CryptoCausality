d033ef0f3bededc007dbca5728e333d1ca714b97
oscar wooding <oscarwooding@gmail.com>
2024-12-16 09:17:11 +0000

    fix folder structure

--- crypto/create_dir_structure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p crypto_causality/data/{raw,processed}
-mkdir -p crypto_causality/src/{data,analysis,utils}
-mkdir -p crypto_causality/notebooks
-mkdir -p crypto_causality/tests
--- python/ewrls/test_python.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-k = 20
-
-x = np.random.randn(k, 1)
-
-print(x.T @ x)
