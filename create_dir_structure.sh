25739a02fc58ebdc22a0813a81ab4253b964b6e0
oscar wooding <oscarwooding@gmail.com>
2024-12-18 10:04:16 +0000

    fix dir structure

--- create_dir_structure.sh
+++ create_dir_structure.sh
@@ -3,4 +3,5 @@ mkdir -p data/{raw,processed}
 mkdir -p src/{data,analysis,utils}
 mkdir -p notebooks
 mkdir -p tests
-mkdir -p results/{1s, 1m, 1h, 1d}
+mkdir -p results
+mkdir -p results/{1s,1m,1h,1d}
