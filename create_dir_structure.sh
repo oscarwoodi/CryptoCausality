73c23fd69edc99f9485bed20dbca95427fda4cdd
Wooding <oscar.wooding@brevanhoward.com>
2025-01-09 09:38:20 +0000

    fix folder structure

--- create_dir_structure.sh
+++ create_dir_structure.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir -p data/{raw,processed,checkpoints}
-mkdir -p data/checkpoints/{1s,1m,1h,1d}}
+mkdir -p data/{raw,processed}
 mkdir -p src/{data,analysis,utils}
 mkdir -p notebooks
 mkdir -p tests
